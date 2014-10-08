d1265a9e8adfce318f9a58e346ff5214a681bcd4
Steffen Krause <slkrause08@gmail.com>
2014-10-08 17:25:41 -0400

    Added 'Method/Function Calling' example demonstrating different ways to call methods/functions

---
+++ src/org/kimbasoft/scala/general/MethodCalling.scala
@@ -0,0 +1,27 @@
+package org.kimbasoft.scala.general
+
+/**
+ * Missing documentation
+ *
+ * @since 1.0
+ */
+object MethodCalling {
+
+  def toCollection(str: String): Seq[String] = str.split(' ')
+
+  def main(args: Array[String]) {
+    val str = "This will be a list of Strings"
+
+    println("-- Type 1 ----------------")
+    toCollection(str).foreach((s:String) => println(s))
+    println("-- Type 2 ----------------")
+    toCollection(str).foreach(s => println(s))
+    println("-- Type 3 ----------------")
+    toCollection(str).foreach(println(_))
+    println("-- Type 4 ----------------")
+    toCollection(str) foreach (println(_))
+    println("-- Type 5 ----------------")
+    toCollection(str) foreach { println(_) }
+
+  }
+}
