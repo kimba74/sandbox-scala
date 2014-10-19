18489752ce678cf2a5fbf1b739596a023262244d
Steffen Krause <steffen.krause@soabridge.com>
2014-10-18 22:53:48 -0400

    Added some more examples

---
+++ src/org/kimbasoft/scala/matching/IfClause.scala
@@ -0,0 +1,27 @@
+package org.kimbasoft.scala.matching
+
+import scala.util.Random
+
+/**
+ * Missing documentation. 
+ *
+ * @author <a href="steffen.krause@soabridge.com">Steffen Krause</a>
+ * @since 1.0
+ */
+object IfClause {
+
+  def main(args: Array[String]) {
+
+    // Regular If-Statement
+    val num1 = Random.nextInt(10)
+    if (num1 <= 5)
+      println(s"$num1 <= 5")
+    else
+      println(s"$num1 > 5")
+
+    // Assignment of If-Statement result
+    val num2 = Random.nextInt(10)
+    val result = if (num2 <= 5) s"$num2 <= 5" else s"$num2 > 5"
+    println(result)
+  }
+}
