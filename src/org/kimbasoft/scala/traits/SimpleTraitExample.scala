1949315c40f8f7fd501198049f836c4e4537b9d1
Steffen Krause <steffen.krause@soabridge.com>
2014-10-26 21:36:07 -0400

    Added additional demonstration to Simple Trait example

--- src/org/kimbasoft/scala/traits/SimpleTraitExample.scala
+++ src/org/kimbasoft/scala/traits/SimpleTraitExample.scala
@@ -8,7 +8,7 @@ package org.kimbasoft.scala.traits
  */
 object SimpleTraitExample {
 
-  class Button(name: String) {
+  class Button(val name: String) {
     def click() {
       // Do some button clicking related stuff
     }
@@ -36,13 +36,29 @@ object SimpleTraitExample {
   }
 
   def main(args: Array[String]) {
-    val button = new ObservableButton("Okay")
-    val observer = new ClickCounter()
+    val button1 = new ObservableButton("Okay")
+    val observer1 = new ClickCounter()
 
-    button.addObserver(observer)
+    button1.addObserver(observer1)
 
-    for (i <- 1 to 3) button.click()
+    for (i <- 1 to 3) button1.click()
 
-    println("> " + observer.counter)
+    println("> " + observer1.counter)
+
+
+    val button2 = new Button("Cancel") with Subject {
+      override def click(): Unit = {
+        super.click()
+        notifyObservers()
+      }
+    }
+    val observer2 = new ClickCounter()
+    button2.addObserver(observer2)
+
+    for (i <- 1 to 5) button2.click()
+
+    println("> " + observer2.counter)
+
+
   }
 }
