67e1aede8f95c9d99c633ccdc50ce6d6c2f0fb76
Steffen Krause <slkrause08@gmail.com>
2014-10-28 10:59:11 -0400

    Added Trait Construction example. Demonstrating the order of class initialization when extending classes and traits

---
+++ src/org/kimbasoft/scala/traits/construction/Example.scala
@@ -0,0 +1,14 @@
+package org.kimbasoft.scala.traits.construction
+
+/**
+ * Missing documentation
+ *
+ * @since 1.0
+ */
+object Example {
+  def main(args: Array[String]) {
+    println("Pre-Creation FinalClass:")
+    val fc = new FinalClass
+    println("Post-Creation FinalClass:")
+  }
+}
