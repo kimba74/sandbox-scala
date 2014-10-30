bc49f61d1461682127728985849edde78ef1b0ac
Steffen Krause <slkrause08@gmail.com>
2014-10-30 16:51:04 -0400

    Added initial Companion Object example

---
+++ src/org/kimbasoft/scala/general/CompanionObjectExample.scala
@@ -0,0 +1,33 @@
+package org.kimbasoft.scala.general
+
+/**
+ * Missing documentation
+ *
+ * @since 1.0
+ */
+object CompanionObjectExample {
+
+  def main(args: Array[String]) {
+    // Default apply() method of Companion Object
+    val comObj1 = CompanionObject()
+    println("Companion Object apply()       => " + comObj1.label)
+
+    // Secondary apply() method of Companion Object with String as argument
+    val comObj2 = CompanionObject("Hello World")
+    println("Companion Object apply(String) => " + comObj2.label)
+
+    // Primary constructor of the actual class
+    val com3 = new CompanionObject("Hello everyone")
+    println("Class Primary Constructor      => " + com3.label)
+
+    // Invocation of the class level apply() method
+    val comIns1 = comObj1(3)
+    println("Class Instance apply()         => '" + comIns1 + "'")
+
+    // Extractor unapply() method of Companion Object
+    comObj2 match {
+      case CompanionObject(label, bool) => println("Companion Object unapply(...)  => label='" + label + "', bool=" + bool)
+      case _ =>  //Do nothing if not CompanionObject
+    }
+  }
+}
