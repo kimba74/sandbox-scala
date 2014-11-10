3d5d4fc26ef9c7918df48e2925a530c572decc59
Steffen Krause <steffen.krause@soabridge.com>
2014-11-09 21:44:31 -0500

    Added more examples for Filtering

--- src/org/kimbasoft/scala/codingstyle/functional/datastructs/Filtering.scala
+++ src/org/kimbasoft/scala/codingstyle/functional/datastructs/Filtering.scala
@@ -16,26 +16,30 @@ object Filtering {
     println("-- Dropping Values -----------------")
     println("Last 5 Values        : " + valueList.drop(5))
     println("First 5 Values       : " + valueList.dropRight(5))
-    println("Drop while value <= 3: " + valueList.dropWhile(num => num <= 3))
+    println("Drop while value <= 3: " + valueList.dropWhile(_ <= 3))
 
     println("-- Exist Values --------------------")
     println("Does number with sqr 25 exist? " + valueList.exists(num => num * num == 25))
     println("Does number with sqr 13 exist? " + valueList.exists(num => num * num == 13))
 
     println("-- Filtering Values ----------------")
-    println("Even Values: " + valueList.filter(num => (num % 2) == 0))
-    println("Odd Values : " + valueList.filter(num => (num % 2) > 0))
+    println("Even Values: " + valueList.filter(_ % 2 == 0))
+    println("Odd Values : " + valueList.filter(_ % 2 > 0))
 
     println("-- Find Values ---------------------")
-    println("Find value > 4: " + valueList.find(num => num > 4))
-    println("Find value < 0: " + valueList.find(num => num < 0))
+    println("Find value > 4: " + valueList.find(_ > 4))
+    println("Find value < 0: " + valueList.find(_ < 0))
 
     println("-- For All Values ------------------")
-    println("All values less 10 : " + valueList.forall(num => num < 10))
-    println("All values are even: " + valueList.forall(num => num % 2 == 0))
+    println("All values less 10 : " + valueList.forall(_ < 10))
+    println("All values are even: " + valueList.forall(_ % 2 == 0))
+
+    println("-- Index of Values ------------------")
+    println("Index of '5' : " + valueList.indexOf(5) )
+    println("Index of '10': " + valueList.indexOf(10))
 
     println("-- Partition Values ----------------")
-    val (multiple, rest) = valueList.partition(num => num % 3 == 0)
+    val (multiple, rest) = valueList.partition(_ % 3 == 0)
     println("Multiple of 3 : " + multiple)
     println("Rest of values: " + rest)
 
