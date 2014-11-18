644bcd06265270c741a94b159bec8d5cc8742bfe
Steffen Krause <steffen.krause@soabridge.com>
2014-11-17 21:55:17 -0500

    Added initial inline documentation for Implicit Evidence example

--- src/org/kimbasoft/scala/codingstyle/functional/implicits/ImplicitEvidence.scala
+++ src/org/kimbasoft/scala/codingstyle/functional/implicits/ImplicitEvidence.scala
@@ -8,6 +8,13 @@ package org.kimbasoft.scala.codingstyle.functional.implicits
  */
 object ImplicitEvidence {
 
+  /**
+   * The object "<:<" ensures that the Type parameter of the set for this class
+   * is of type String. This is a simple example of how a method can assure at
+   * compile time that it is invoked on valid types only.
+   * String is a simplification to demonstrate the functionality of the "<:<"
+   * object. A better example would probably be a Tuple (A, B).
+   */
   class Processor[T](val obj: T) {
     def process(implicit ev: <:<[T, String]) = "<" + obj.replace(' ', '_') + ">"
   }
