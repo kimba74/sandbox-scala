aeac401ced2a08f7d49a065dcac9c67b34faf6d1
Steffen Krause <slkrause08@gmail.com>
2014-12-02 09:08:36 -0500

    Fixed Future Callbacks example (added Thread.sleep)

--- src/org/kimbasoft/scala/concurrency/FutureExample.scala
+++ src/org/kimbasoft/scala/concurrency/FutureExample.scala
@@ -3,7 +3,7 @@ package org.kimbasoft.scala.concurrency
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{Failure, Success, Try}
+import scala.util.{Random, Failure, Success, Try}
 
 /**
  * Missing documentation
@@ -29,21 +29,26 @@ object FutureExample {
     }
   }
 
-  /** !!!! NOT WORKING YET !!!! */
   object CallbackFuture {
-    case class ThatsOdd(i: Int) extends RuntimeException(s"odd $i received!")
+    // Exception object required by Future.failed()
+    case class OddBall(i: Int) extends RuntimeException(s"odd $i received!")
 
     def run {
-      println("-- Callback Future Example -----------------")
+      println("-- Future Callbacks Example ----------------")
+      // Create callback function for onComplete() method
       val doComplete: PartialFunction[Try[String],Unit] = {
         case s @ Success(_) => println(s)
         case f @ Failure(_) => println(f)
       }
+      // Create Seq of Future objects
       val futures = (0 to 9) map {
         case i if i % 2 == 0 => Future.successful(i.toString)
-        case i => Future.failed(ThatsOdd(i))
+        case i => Future.failed(OddBall(i))
       }
+      // Adding callback function 'doComplete' for completed Futures
       futures map (_ onComplete doComplete)
+      // Giving Future objects time to complete and invoke callback
+      Thread.sleep(1000)
     }
   }
 
