3b2a3da720c2ebd9711f91baa416a1807d0888a5
Steffen Krause <slkrause08@gmail.com>
2014-12-03 18:06:05 -0500

    Added some inline comments to Simple Future example

--- src/org/kimbasoft/scala/concurrency/FutureSimple.scala
+++ src/org/kimbasoft/scala/concurrency/FutureSimple.scala
@@ -14,6 +14,9 @@ object FutureSimple {
   def main(args: Array[String]) {
     println("-- Simple Future Example -------------------")
     print("Future: ")
+    /* Creating a list of individual futures by running map() on a specified
+     * Range. We then implement a new Future object by passing the current
+     * number in, printing, and returning it. */
     val futures = (0 to 9) map {
       i => Future {
         val s = i.toString
@@ -21,6 +24,9 @@ object FutureSimple {
         s
       }
     }
+    /* Collapsing all the 10 individual Futures into one so we can check,
+     * when all of them are done. The collapsed future will only be done
+     * when each individual future is done. */
     val future = Future.reduce(futures)((s1, s2) => s1 + s2)
     val result = Await.result(future, Duration.Inf)
     println("\nResult: " + result)
