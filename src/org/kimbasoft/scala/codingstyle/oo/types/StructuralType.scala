abd3a1db81857353e71b9e41efe2f33c2a48ca2d
Steffen Krause <steffen.krause@soabridge.com>
2014-12-11 22:20:53 -0500

    Added another Structural Type use-case to Types examples

--- src/org/kimbasoft/scala/codingstyle/oo/types/StructuralType.scala
+++ src/org/kimbasoft/scala/codingstyle/oo/types/StructuralType.scala
@@ -10,14 +10,22 @@ object StructuralType {
   object Registry {
     // Import flag to enable Reflective Calls for Structural Types
     import scala.language.reflectiveCalls
-    // The Structural Type
+    // Structural Type describing an expected Object's structure
     type Applicant = { def apply(): String }
+    // Structural Type describing a function
+    type Descriptor = () => String
     
     private var applicants: List[Applicant] = Nil 
-    
+
+    private var descriptors: List[Descriptor] = Nil
+
     def register(applicant: Applicant) = applicants ::= applicant
 
+    def describe(descriptor: Descriptor) = descriptors ::= descriptor
+
     def printApplications() = applicants foreach (a => println(a.apply()))
+
+    def printDescriptions() = descriptors foreach (d => println(d()))
   }
 
   case class SchoolApplicant(name: String) {
@@ -28,14 +36,25 @@ object StructuralType {
     def apply(): String = s"Passport application for $name in $country"
   }
 
+  case class BookDescription(summary: String)
+
+  case class JobDescription(position: String)
+
 
   def main(args: Array[String]) {
+    println("-- Using an Object Structural Type -----------")
     Registry.register(SchoolApplicant("John Doe"))
-    Registry.register(SchoolApplicant("Jane Doe"))
     Registry.register(PassportApplicant("Mark Smith", "Great Britain"))
-    Registry.register(SchoolApplicant("Frank Boyle"))
+    Registry.register(SchoolApplicant("Jane Doe"))
     Registry.register(PassportApplicant("Nancy Baker", "USA"))
-
     Registry.printApplications()
+
+    println("-- Using a Function Structural Type ----------")
+    Registry.describe(() => BookDescription("Map of Bones is a great book!").summary)
+    Registry.describe(() => JobDescription("Working as a lumberjack is tough work").position)
+    val funct = () => "This is just a function"
+    Registry.describe(funct)
+    Registry.describe(() => "And this is an anonymous function")
+    Registry.printDescriptions()
   }
 }
