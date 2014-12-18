2a08f9442f1acfa6cbfe5664b24574c066901ddd
Steffen Krause <steffen.krause@soabridge.com>
2014-12-17 21:31:06 -0500

    Added more inline documentation to Compound Type use case in Types examples.

--- src/org/kimbasoft/scala/codingstyle/oo/types/CompoundType.scala
+++ src/org/kimbasoft/scala/codingstyle/oo/types/CompoundType.scala
@@ -7,15 +7,33 @@ package org.kimbasoft.scala.codingstyle.oo.types
  */
 object CompoundType {
 
+  /**
+   * Abstract base class. This class will be used as the basis of the
+   * new compound type. This does not have to be an abstract class, it
+   * can also be a concrete class or another trait.
+   */
+  abstract class World(val name: String = "World")
+
+  /**
+   * Mixin Trait extending the abstract base class 'World'. This extension
+   * is not required for Compound Types but in this example the traits
+   * make use of the 'name' field of the base class and therefore have to
+   * extend the base class.
+   */
   trait Greeting extends World {
     def sayHello() = println(s"Hello $name!")
   }
 
+  /**
+   * Mixin Trait extending the abstract base class 'World'. This extension
+   * is not required for Compound Types but in this example the traits
+   * make use of the 'name' field of the base class and therefore have to
+   * extend the base class.
+   */
   trait GoodBye extends World {
     def sayGoodBye() = println(s"Good-Bye, $name!")
   }
 
-  class World(val name: String = "World")
 
   /**
    * Regular type extending another type and mixing in some traits.
