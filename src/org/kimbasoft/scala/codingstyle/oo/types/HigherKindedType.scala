650b214c64662b1960265c6dc4bc8f6c46ff5dae
Steffen Krause <steffen.krause@soabridge.com>
2014-12-25 23:40:56 -0500

    Added improvement to Higher-Kinded Types to Types examples

--- src/org/kimbasoft/scala/codingstyle/oo/types/HigherKindedType.scala
+++ src/org/kimbasoft/scala/codingstyle/oo/types/HigherKindedType.scala
@@ -24,11 +24,15 @@ object HigherKindedType {
     }
   }
 
+  def sumSeq[T : Add](seq: Seq[T]): T = {
+    seq reduce implicitly[Add[T]].add
+  }
+
   /**
    * The Reduce trait functions as an abstraction of reduction (reduce(...) method)
    * for higher-kinded types. Using M as parameter name is an informal convention.
    */
-  trait Reduce[T, -M[T]] {
+  trait Reduce1[T, -M[T]] {
     def reduce(m: M[T])(f: (T,T) => T): T
   }
 
@@ -36,17 +40,13 @@ object HigherKindedType {
    * Object that defines implicit instances of the Reduce Trait for reducing Seq
    * and Option values.
    */
-  object Reduce {
-    implicit def reduceSeq[T]: Reduce[T, Seq] = new Reduce[T, Seq] {
+  object Reduce1 {
+    implicit def reduceSeq[T]: Reduce1[T, Seq] = new Reduce1[T, Seq] {
       def reduce(seq: Seq[T])(f: (T, T) => T): T = seq reduce f
     }
-    implicit def reduceOption[T]: Reduce[T, Option] = new Reduce[T, Option] {
+    implicit def reduceOption[T]: Reduce1[T, Option] = new Reduce1[T, Option] {
       def reduce(opt: Option[T])(f: (T, T) => T): T = opt reduce f
     }
-  }
-
-  def sumSeq[T : Add](seq: Seq[T]): T = {
-    seq reduce implicitly[Add[T]].add
   }
 
   /**
@@ -55,8 +55,26 @@ object HigherKindedType {
    * is only valid if an implicit Add[T] object exists (Context Binding) within the
    * context otherwise this code will not compile.
    */
-  def sumAll[T : Add,M[T]](container: M[T])(implicit red: Reduce[T,M]): T = {
+  def sumReduce1[T : Add,M[T]](container: M[T])(implicit red: Reduce1[T,M]): T = {
     red.reduce(container)(implicitly[Add[T]].add)
+  }  
+  
+  
+  trait Reduce2[-M[_]] {
+    def reduce[T](m: M[T])(f: (T,T) => T): T
+  }
+
+  object Reduce2 {
+    implicit val reduce2Seq = new Reduce2[Seq] {
+      def reduce[T](seq: Seq[T])(f: (T, T) => T): T = seq reduce f
+    }
+    implicit val reduce2Option = new Reduce2[Option] {
+      def reduce[T](option: Option[T])(f: (T, T) => T): T = option reduce f
+    }
+  }
+
+  def sumReduce2[T : Add, M[_] : Reduce2](container: M[T]): T = {
+    implicitly[Reduce2[M]].reduce(container)(implicitly[Add[T]].add)
   }
 
   def main(args: Array[String]) {
@@ -67,9 +85,14 @@ object HigherKindedType {
 //  println(sumSeq(Option(2)))             // Does not compile since Option is not a sub-type of Seq
 
     println("-- Higher-Kinded Type ------------------------")
-    println(sumAll(Vector(2 -> 20, 3 -> 30, 4 -> 40)))
-    println(sumAll(2 to 20))
-    println(Option(2))
+    println(sumReduce1(Vector(2 -> 20, 3 -> 30, 4 -> 40)))
+    println(sumReduce1(2 to 20))
+    println(sumReduce1(Option(2)))
+
+    println("-- Higher-Kinded Type Improved ---------------")
+    println(sumReduce2(Vector(3 -> 30, 4 -> 40, 5 -> 50)))
+    println(sumReduce2(3 to 30))
+    println(sumReduce2(Option(3)))
   }
 
 }
