42f536ca3ee95e0adfef3a937146140b8ffbd0a5
Steffen Krause <steffen.krause@soabridge.com>
2014-12-26 23:40:24 -0500

    Added documentation to Higher-Kinded Types in Types examples

--- src/org/kimbasoft/scala/codingstyle/oo/types/HigherKindedType.scala
+++ src/org/kimbasoft/scala/codingstyle/oo/types/HigherKindedType.scala
@@ -11,10 +11,17 @@ import scala.language.higherKinds
  */
 object HigherKindedType {
 
+  /**
+   * Abstract Trait 'Add' that defines an 'add()' method for the defined type T
+   */
   trait Add[T] {
     def add(t1: T, t2: T): T
   }
 
+  /**
+   * Companion object to trait 'Add' that provides implicit implementations of the
+   * abstract Trait for the types 'Int' and Tuple2 '(Int,Int)'
+   */
   object Add {
     implicit val addInt = new Add[Int] {
       def add(t1: Int, t2: Int): Int = t1 + t2
@@ -24,6 +31,16 @@ object HigherKindedType {
     }
   }
 
+  /**
+   * Summing method that takes arguments of type 'Seq' that are parametrized with type T.
+   * The type T is context bound and requires an implementation Add for type T to be present
+   * in the current context, is the object not present the code will not compile. The
+   * method will implicitly call the Add[T] object and invoke the add() method on it.
+   * This method will only allow Seq sub-types to be used and will not allow custom collections
+   * not part of the Seq sub-type system. This is highly undesirable since a new method
+   * must be implemented for any collection sub-type. The Reduce1 example will explore
+   * a more flexible approach to that problem.
+   */
   def sumSeq[T : Add](seq: Seq[T]): T = {
     seq reduce implicitly[Add[T]].add
   }
@@ -37,8 +54,8 @@ object HigherKindedType {
   }
 
   /**
-   * Object that defines implicit instances of the Reduce Trait for reducing Seq
-   * and Option values.
+   * Companion Object for Reduce1 Trait that defines implicit instances of the Trait
+   * for reducing Seq and Option values.
    */
   object Reduce1 {
     implicit def reduceSeq[T]: Reduce1[T, Seq] = new Reduce1[T, Seq] {
@@ -51,19 +68,41 @@ object HigherKindedType {
 
   /**
    * This method requires a container object of type M that holds a value of type T
-   * and an implicit Reduce object with the types T and M. Furthermore, the type T
-   * is only valid if an implicit Add[T] object exists (Context Binding) within the
-   * context otherwise this code will not compile.
+   * and an implicit Reduce1 object parametrized with the types T and M. Furthermore,
+   * the type T is only valid if an implicit Add[T] object exists (Context Binding)
+   * within the context otherwise this code will not compile.
+   * This approach lets us use the Add Trait on custom collections and other container
+   * objects that were not accepted by the sumSeq() method due to the fact that they
+   * were not sub-types of Seq. However, this approach is a bit ugly. We would rather
+   * have the Reduce1 object to be context bound rather than having to pass it in via
+   * 'implicit' in a second argument list. With the Reduce1 approach this cannot be
+   * accomplished since the Reduce1 Trait has two parameters rather than one. The
+   * Reduce2 approach will try to address this issue.
    */
   def sumReduce1[T : Add,M[T]](container: M[T])(implicit red: Reduce1[T,M]): T = {
     red.reduce(container)(implicitly[Add[T]].add)
-  }  
-  
-  
+  }
+
+  /**
+   * The Reduce2 Trait is very similar to the Reduce1 Trait in that it defines an
+   * abstract 'reduce()' method. The main difference is that the value parameter T
+   * has been moved from the trait to the method definition. This change however
+   * prevents the use of the value parameter T in the container parameter M declaration.
+   * The container parameter M will therefore be declared as an Existential Type.
+   */
   trait Reduce2[-M[_]] {
     def reduce[T](m: M[T])(f: (T,T) => T): T
   }
 
+  /**
+   * The Companion Object of the Reduce2 trait provides implicit instances of Trait
+   * for the Seq and Option containers. Other than its sibling the Reduce1 companion
+   * object this object defines the implicit instances as 'val' rather than as
+   * factory method. This was made possible by moving the value parameter T from the
+   * trait and moving it to the method declaration thus eliminating the need for
+   * having a variable parameter around. The value parameter T will not be required
+   * until the Reduce2.reduce() method is actually invoked.
+   */
   object Reduce2 {
     implicit val reduce2Seq = new Reduce2[Seq] {
       def reduce[T](seq: Seq[T])(f: (T, T) => T): T = seq reduce f
@@ -73,6 +112,16 @@ object HigherKindedType {
     }
   }
 
+  /**
+   * The change in the Reduce2 trait definition and the elimination of the value
+   * parameter T allows us to context bind the container parameter M and remove
+   * the ugly second argument list with the implicit Reduce argument. Now the
+   * Reduce2.reduce() method can be invoked using 'implicitly' like we used for
+   * the Add.add() method from the beginning.
+   * This approach now elegantly disconnected the container from its value type
+   * and thus permits us handle container implementations separately from their
+   * value implementations in a flexible, extensible, and elegant manner.
+   */
   def sumReduce2[T : Add, M[_] : Reduce2](container: M[T]): T = {
     implicitly[Reduce2[M]].reduce(container)(implicitly[Add[T]].add)
   }
