bd17b8b5950e42425e785c62c1db043a06d073d2
Steffen Krause <steffen.krause@soabridge.com>
2014-12-30 22:27:34 -0500

    Added inline documentation to Self-Recursive Type use case

--- src/org/kimbasoft/scala/codingstyle/oo/types/SelfRecursiveType.scala
+++ src/org/kimbasoft/scala/codingstyle/oo/types/SelfRecursiveType.scala
@@ -25,16 +25,19 @@ object SelfRecursiveType {
   }
 
   def main(args: Array[String]) {
+    // Creating the root objects from the Child1 and Child2 implementations
     val c1 = Child1("c1")
     val c2 = Child2("c2")
     println(s"c1 = $c1")
     println(s"c2 = $c2")
 
+    // Creating children objects from the Child1 and Child2 root objects using the make() method
     val c11 = c1.make
     val c22 = c2.make
     println(s"c11 = $c11")
     println(s"c22 = $c22")
 
+    // Assigning the Child1 and Child2 root objects to variables of the Parent trait type
     val p1: Parent[Child1] = c1
     val p2: Parent[Child2] = c2
     println(s"p1 = $p1")
