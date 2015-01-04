0fa5a6c1a4ba307c766fca1ee28e23539378482c
Steffen Krause <steffen.krause@soabridge.com>
2015-01-03 19:58:24 -0500

    Added inline documentation to Implicit Type Erasure use case

--- src/org/kimbasoft/scala/codingstyle/functional/implicits/ImplicitTypeErasure.scala
+++ src/org/kimbasoft/scala/codingstyle/functional/implicits/ImplicitTypeErasure.scala
@@ -12,6 +12,16 @@ object ImplicitTypeErasure {
 
   implicit object StringMarker
 
+  /**
+   * This object defines two methods that, aside from the type parameters are
+   * absolutely identical. Type parameters are mainly used to guide developers.
+   * Due to Type Erasure during compilation the compiler will not allow this kind
+   * of method overloading. However, Scala permits a workaround to make the methods
+   * different from each other, this mechanism is called implicit evidence for
+   * type erasure. To used this workaround one simply defines implicit "marker"
+   * objects and appends the method with a second parameter list taking an implicit
+   * argument of the marker types.
+   */
   object SeqPrinter {
     def print(seq: Seq[Int])(implicit ev: IntMarker.type) = println("Integer Seq: " + seq)
 
