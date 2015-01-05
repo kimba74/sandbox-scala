78e365299df74cffe9ceb643715b5eda9dbd9d07
Steffen Krause <steffen.krause@soabridge.com>
2015-01-04 21:39:07 -0500

    Added inline documentation to Implicitly use case

--- src/org/kimbasoft/scala/codingstyle/functional/implicits/Implicitly.scala
+++ src/org/kimbasoft/scala/codingstyle/functional/implicits/Implicitly.scala
@@ -21,14 +21,25 @@ object Implicitly {
 
   class Printer {
     /**
-     * Use of implicit argument
+     * In order to get a suitable Decorator object for type T this method
+     * adds a second argument list with an implicit argument for the Decorator.
+     * Since the decorator object is directly injected into a named variable
+     * in can be used directly. However, the use of a second argument list
+     * for this case can be a bit ugly.
      */
     def print1[T](obj: T)(implicit decorator: Decorator[T]): Unit = {
       println(decorator.decorate(obj))
     }
 
     /**
-     * Use of implicitly() via context binding
+     * To avoid the ugliness of a second argument list this method performs a
+     * Context Binding on type T to Decorator. In other words, it prevents types
+     * from being used as T for which no Decorator object exists in the current
+     * context. Since this Context Binding removes the second argument it is
+     * not possible to access the decorator object directly anymore. This is where
+     * the 'implicitly[]' method comes into play. It will be parametrized with the
+     * object that type T is bound to, look it up and return its instance thus
+     * allowing for the invocation of the object's methods.
      */
     def print2[T : Decorator](obj: T) = {
       println(implicitly[Decorator[T]].decorate(obj))
