784470f23a24f3549c4a4e9150686e2795c0d0a4
Steffen Krause <steffen.krause@soabridge.com>
2015-01-12 21:49:39 -0500

    Added initial classes for Akka Test Use Case

---
+++ src/org/kimbasoft/akka/usecase1/MyClient.scala
@@ -0,0 +1,17 @@
+package org.kimbasoft.akka.usecase1
+
+import akka.actor.ActorSystem
+
+/**
+ * Missing documentation. 
+ *
+ * @author <a href="steffen.krause@soabridge.com">Steffen Krause</a>
+ * @since 1.0
+ */
+object MyClient {
+
+  def main(args: Array[String]) {
+    val sys = ActorSystem("My Actors")
+  }
+
+}
