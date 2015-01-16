515d321b816b08ec37fc9649c4836b7eab246456
Steffen Krause <steffen.krause@soabridge.com>
2015-01-15 22:37:45 -0500

    Added some more inline documentation of MyActor

--- src/org/kimbasoft/akka/usecase1/MyActor.scala
+++ src/org/kimbasoft/akka/usecase1/MyActor.scala
@@ -1,6 +1,6 @@
 package org.kimbasoft.akka.usecase1
 
-import akka.actor.{Props, Actor}
+import akka.actor.{SupervisorStrategy, Props, Actor}
 import akka.pattern.ask
 
 /**
@@ -11,6 +11,18 @@ import akka.pattern.ask
  */
 class MyActor extends Actor {
 
+  /**
+   * Method that defines the Supervisor Strategy for all Child Actors.
+   */
+  override def supervisorStrategy: SupervisorStrategy = {
+    super.supervisorStrategy
+  }
+
+  /**
+   * Method that defines the behavior of the Actor when receiving a message.
+   * This method can be changed at runtime using the 'become()' method on
+   * the ActorContext (use 'context' variable of Actor).
+   */
   def receive: Receive = {
     /* Creating an Actor of type 'MyActor' as sub-actor of this supervising Actor.
      * Child-Level Actors will be created via the actorOf() method of the ActorContext
