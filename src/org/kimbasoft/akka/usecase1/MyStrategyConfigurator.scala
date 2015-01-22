bef2e28fe6c9c38c50d8fbf0e88b9bbcfe3be586
Steffen Krause <steffen.krause@soabridge.com>
2015-01-21 21:30:56 -0500

    Added some more inline documentation about SupervisorStrategy Directives

--- src/org/kimbasoft/akka/usecase1/MyStrategyConfigurator.scala
+++ src/org/kimbasoft/akka/usecase1/MyStrategyConfigurator.scala
@@ -12,6 +12,13 @@ import akka.actor.{OneForOneStrategy, SupervisorStrategy, SupervisorStrategyConf
 class MyStrategyConfigurator extends SupervisorStrategyConfigurator {
   /* Implementations of SupervisorStrategyConfigurator must be of type 'class' */
   def create(): SupervisorStrategy = OneForOneStrategy() {
+    /* The Matching must return a SupervisorStrategy Directive.
+     * Since Directive is a sealed Trait only the following options are possible:
+     *    Resume   - Resumes Message processing for the failed Actor
+     *    Restart  - Discards the old Actor instance and replaces it with a new, then resumes message processing.
+     *    Stop     - Stops the failed Actor
+     *    Escalate - Escalates the problem to the next higher supervisor
+     */
     case MyActor.ProcessingException => Resume
     case MyActor.GeneralException => Restart
     case _: MyActor.TestException => Stop
