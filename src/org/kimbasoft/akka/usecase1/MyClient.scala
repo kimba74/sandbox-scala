59464495195fe849a880c8583594eab116d989ae
Steffen Krause <steffen.krause@soabridge.com>
2015-01-29 22:26:34 -0500

    Added some initial Client invocation logic

--- src/org/kimbasoft/akka/usecase1/MyClient.scala
+++ src/org/kimbasoft/akka/usecase1/MyClient.scala
@@ -1,6 +1,7 @@
 package org.kimbasoft.akka.usecase1
 
 import akka.actor._
+import org.kimbasoft.akka.usecase1.MyActorMessages.{ProcessSummation, ProcessFactorial}
 
 /**
  * Missing documentation. 
@@ -22,6 +23,16 @@ object MyClient {
     /* Creating an Actor of type 'MyActor' in the previously created Actor System.
      * Top-Level Actors will be created via the actorOf() method of the Actor System. */
     val mainActor = sys.actorOf(Props[MyActor], "MainActor")
+
+    /* Creating some sample data for the Actor to process and then passing the content
+     * as payload of a processing instruction message to the Actor. */
+    // Creating the sample data
+    val seq1 = Seq(1, 2, 3, 4, 5, 6)
+    val seq2 = Seq(4, 5, 6, 7, 8, 9)
+
+    // Sending the sample data as payload of a message to the Actor
+    mainActor ! ProcessFactorial(seq1)
+    mainActor ! ProcessSummation(seq2)
   }
 
 }
