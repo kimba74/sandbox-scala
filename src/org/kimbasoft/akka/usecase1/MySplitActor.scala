8f4212c5a9fe718f64545c09afe490211aaf197e
Steffen Krause <steffen.krause@soabridge.com>
2015-02-11 22:03:11 -0500

    MySplitActor now returns a message to its parent

--- src/org/kimbasoft/akka/usecase1/MySplitActor.scala
+++ src/org/kimbasoft/akka/usecase1/MySplitActor.scala
@@ -2,10 +2,10 @@ package org.kimbasoft.akka.usecase1
 
 import akka.actor.SupervisorStrategy.Stop
 import akka.actor._
-import org.kimbasoft.akka.usecase1.MyActorMessages.{SplitResponse, SplitRequest}
+import org.kimbasoft.akka.usecase1.MyActorMessages.{SplitRequest, SplitResponse}
 import org.kimbasoft.akka.usecase1.MySplitActor.InvalidRequestException
 
-import scala.util.{Success, Failure}
+import scala.util.Success
 
 /**
  * Missing documentation. 
@@ -23,6 +23,7 @@ class MySplitActor(name: String) extends Actor {
 
   override def receive: Receive = {
     case SplitRequest(depth, message) =>
+      // If the desired depth has not been reached yet spin off two new actors and pass on the message
       if(depth > 0) {
         println(s">> $name: $message")
         context.actorOf(Props(classOf[MySplitActor], s"$name.$count"), s"$name.$count") ! SplitRequest(depth - 1, message)
@@ -30,12 +31,14 @@ class MySplitActor(name: String) extends Actor {
         context.actorOf(Props(classOf[MySplitActor], s"$name.$count"), s"$name.$count") ! SplitRequest(depth - 1, message)
         count += 1
       }
+      // If message has reached the furthest depth return message to actor's parent
       else {
         println(s"== $name: $message")
+        context.parent ! SplitResponse(Success(message))
       }
     case res @ SplitResponse(result) =>
       println(s"<< $name: $result")
-      sender ! res
+      context.parent ! res
     case unknown =>
       println(s"Oops! [$unknown]")
   }
