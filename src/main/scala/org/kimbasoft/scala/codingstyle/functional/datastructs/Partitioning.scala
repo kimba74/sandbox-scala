db05ac9674eb040ac87a90c783e04ab4612570e9
Steffen Krause <steffen.krause@soabridge.com>
2015-03-24 21:09:18 -0400

    Moved Collection Partitioning example and updated it as well

---
+++ src/main/scala/org/kimbasoft/scala/codingstyle/functional/datastructs/Partitioning.scala
@@ -0,0 +1,22 @@
+package org.kimbasoft.scala.codingstyle.functional.datastructs
+
+/**
+ * @author <a href="steffen.krause@soabridge.com">Steffen Krause</a>
+ * @since 1.0
+ */
+object Partitioning {
+
+  case class Person(name:String, age:Int)
+
+  def main(args: Array[String]) {
+    val people = Array(Person("Bob", 40), Person("Edith", 53), Person("John", 4), Person("Megan", 17))
+
+    val (minors, adults) = people partition(_.age < 18)
+
+    println("-- Minors -----------------")
+    minors foreach println
+    println("-- Adults -----------------")
+    adults foreach println
+  }
+}
+
