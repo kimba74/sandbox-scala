48136b2dd23e1f1b4a8ed34436e66d95ec690ac2
Steffen Krause <steffen.krause@soabridge.com>
2015-04-07 21:41:20 -0400

    Added more documentation to Unit example

--- src/main/scala/org/kimbasoft/specs2/structure/UnitSpec.scala
+++ src/main/scala/org/kimbasoft/specs2/structure/UnitSpec.scala
@@ -4,11 +4,23 @@ package org.kimbasoft.specs2.structure
 import org.specs2.mutable.Specification
 
 /**
- * Missing documentation
+ * The Unit style specification extends 'org.specs2.mutable.Specification' and operates
+ * in the Thrown Expectation mode. This means the first failure encountered in a block
+ * of expectations throws an exception, which will be interpreted as the block's final result.
  *
- * @since 1.0
+ * Example:
+ *        def exp = {
+ *           1 must be equalTo 4
+ *           3 must beGreaterThan 1
+ *        }
+ *
+ * The first statement in the block is 'false', the block will thrown an exception, and the
+ * the last statement of the block will never be evaluated since the result has already been
+ * given. This behavior can be altered by changing the Specification's default Thrown Expectation
+ * to the Functional Expectation behavior. The behavior change can be achieved by also implementing
+ * the 'org.specs2.matcher.NoThrownExpectations' trait.
  */
-class UnitSpec extends Specification {
+class UnitSpec extends Specification {   // with NoThrownExpectations
   "This is my specification" >> {
     "where example 1 must be true" >> {
       1 must_== 1
