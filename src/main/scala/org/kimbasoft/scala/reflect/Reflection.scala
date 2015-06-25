b9661a8cb45fa325653afb895f90a1905ae7f00c
Steffen Krause <steffen.krause@soabridge.com>
2015-06-24 23:09:34 -0400

    Added dynamic class loading example to reflection

---
+++ src/main/scala/org/kimbasoft/scala/reflect/Reflection.scala
@@ -0,0 +1,29 @@
+package org.kimbasoft.scala.reflect
+
+import scala.reflect.runtime.{universe => ru}
+
+/**
+ * Missing documentation. 
+ *
+ * @author <a href="steffen.krause@soabridge.com">Steffen Krause</a>
+ * @since 1.0
+ */
+object Reflection extends App {
+
+  // Getting the runtime mirror for the provided ClassLoader
+  val runMirror = ru.runtimeMirror(getClass.getClassLoader)
+
+  // Loading a specified class based on the class' fully qualified name
+  val classTest   = runMirror.staticClass("org.kimbasoft.scala.reflect.TestClass")
+  val classMirror = runMirror.reflectClass(classTest)
+
+  // Loading the primary constructor of the loaded class
+  val constTest   = classTest.primaryConstructor.asMethod
+  val constMirror = classMirror.reflectConstructor(constTest)
+
+  // Create instance of loaded class via primary constructor
+  val instTest = constMirror("MyTest")
+
+  // Invoke toString() on dynamically loaded class
+  println(instTest)
+}
---
+++ src/main/scala/org/kimbasoft/scala/reflect/TestClass.scala
@@ -0,0 +1,19 @@
+package org.kimbasoft.scala.reflect
+
+/**
+ * Missing documentation. 
+ *
+ * @author <a href="steffen.krause@soabridge.com">Steffen Krause</a>
+ * @since 1.0
+ */
+class TestClass(name: String) {
+  override def toString: String = s"This is TestClass($name)"
+}
+
+object TestObject {
+  override def toString: String = "This is TestObject"
+}
+
+case class TestCase(name: String) {
+  override def toString: String = s"This is TestCase($name)"
+}
