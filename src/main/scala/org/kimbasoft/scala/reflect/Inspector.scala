1179d5b0da56e3f34e5636b732ff611402b252e8
Steffen Krause <steffen.krause@soabridge.com>
2015-07-04 22:39:36 -0400

    Restructured class inspection

---
+++ src/main/scala/org/kimbasoft/scala/reflect/Inspector.scala
@@ -0,0 +1,92 @@
+package org.kimbasoft.scala.reflect
+
+import scala.reflect.runtime.{universe => ru}
+
+/**
+ * Missing documentation. 
+ *
+ * @author <a href="steffen.krause@soabridge.com">Steffen Krause</a>
+ * @since 1.0
+ */
+object Inspector {
+  def inspect(sym: ru.Symbol, indent: String = "") = {
+    sym match {
+      case c: ru.ClassSymbol  => inspectClass(c, indent)
+      case m: ru.ModuleSymbol => inspectModule(m, indent)
+      case m: ru.MethodSymbol => inspectMethod(m, indent)
+      case t: ru.TermSymbol   => inspectTerm(t, indent)
+      case t: ru.TypeSymbol   => inspectType(t, indent)
+      case u                  => println(s"Unknown Symbol: $u")
+    }
+  }
+
+  private def inspectClass(sym: ru.ClassSymbol, indent: String = ""): Unit = {
+    println(indent + sym)
+    for (member <- sym.info.decls) {
+      inspect(member, indent + "  ")
+    }
+    // Inspect Companion Object if existing
+    if (sym.companion != ru.NoSymbol)
+      inspect(sym.companion)
+  }
+
+  private def inspectModule(sym: ru.ModuleSymbol, indent: String = ""): Unit = {
+    println(indent + sym)
+    for (member <- sym.info.decls)
+      inspect(member, indent + "  ")
+  }
+
+  private def inspectMethod(sym: ru.MethodSymbol, indent: String = ""): Unit = {
+    // Determine visibility
+    var visible: String = ""
+    sym match {
+      case p if p.isPrivate   => visible = "private "
+      case p if p.isProtected => visible = "protected "
+      case _ => visible = ""
+    }
+    // Determine method type
+    var prefix: String = ""
+    sym match {
+      case c if c.isConstructor => prefix = "constructor "
+      case s if s.isSetter      => prefix = "setter "
+      case g if g.isGetter      => prefix = "getter "
+      case _ => prefix = "def "
+    }
+    // Determine name
+    val name = sym.name.decodedName
+    //
+    var params = ""
+    for (paramList <- sym.info.paramLists) {
+      params = params + "("
+      for (param <- paramList)
+        params = params + param.name.decodedName + ": " + param.typeSignature + " "
+      params = params + ")"
+    }
+    // Determine return value
+    val retVal = sym.returnType
+
+    println(indent + visible + prefix + name + params + ": " + retVal)
+    //    for (member <- sym.info.decls)
+    //      inspect(member, indent + "  ")
+  }
+
+  private def inspectTerm(sym: ru.TermSymbol, indent: String = ""): Unit = {
+    // Determine type
+    var termType = ""
+    sym match {
+      case v if sym.isVal => termType = "val "
+      case v if sym.isVar => termType = "var "
+      case _ => termType = "??? "
+    }
+    // Determine name
+    val name = sym.name.decodedName
+    // Determine type
+    val retVal = sym.typeSignature
+
+    println(indent + termType + name + ": " + retVal)
+  }
+
+  private def inspectType(sym: ru.TypeSymbol, indent: String = ""): Unit = {
+    println(indent + sym)
+  }
+}
--- src/main/scala/org/kimbasoft/scala/reflect/Reflection.scala
+++ src/main/scala/org/kimbasoft/scala/reflect/Reflection.scala
@@ -21,8 +21,6 @@ object Reflection extends App {
   val constTest   = classTest.primaryConstructor.asMethod
   val constMirror = classMirror.reflectConstructor(constTest)
 
-  inspect(classTest)
-
   // Inspecting the default constructor
   println("Inspecting constructor parameters")
   for(symList <- constTest.info.paramLists) {
@@ -36,85 +34,7 @@ object Reflection extends App {
   // Invoke toString() on dynamically loaded class
   println(instTest)
 
+  // Using Inspector to inspect class
+  Inspector.inspect(classTest)
 
-  def inspect(sym: ru.Symbol, indent: String = "") = {
-    sym match {
-      case c: ru.ClassSymbol  => inspectClass(c, indent)
-      case m: ru.ModuleSymbol => inspectModule(m, indent)
-      case m: ru.MethodSymbol => inspectMethod(m, indent)
-      case t: ru.TermSymbol   => inspectTerm(t, indent)
-      case t: ru.TypeSymbol   => inspectType(t, indent)
-      case u                  => println(s"Unknown Symbol: $u")
-    }
-  }
-
-  def inspectClass(sym: ru.ClassSymbol, indent: String = ""): Unit = {
-    println(indent + sym)
-    for (member <- sym.info.decls) {
-      inspect(member, indent + "  ")
-    }
-    // Inspect Companion Object if existing
-    if (sym.companion != ru.NoSymbol)
-      inspect(sym.companion)
-  }
-
-  def inspectModule(sym: ru.ModuleSymbol, indent: String = ""): Unit = {
-    println(indent + sym)
-    for (member <- sym.info.decls)
-      inspect(member, indent + "  ")
-  }
-
-  def inspectMethod(sym: ru.MethodSymbol, indent: String = ""): Unit = {
-    // Determine visibility
-    var visible: String = ""
-    sym match {
-      case p if p.isPrivate   => visible = "private "
-      case p if p.isProtected => visible = "protected "
-      case _ => visible = ""
-    }
-    // Determine method type
-    var prefix: String = ""
-    sym match {
-      case c if c.isConstructor => prefix = "constructor "
-      case s if s.isSetter      => prefix = "setter "
-      case g if g.isGetter      => prefix = "getter "
-      case _ => prefix = "def "
-    }
-    // Determine name
-    val name = sym.name.decodedName
-    //
-    var params = ""
-    for (paramList <- sym.info.paramLists) {
-      params = params + "("
-      for (param <- paramList)
-        params = params + param.name.decodedName + ": " + param.typeSignature + " "
-      params = params + ")"
-    }
-    // Determine return value
-    val retVal = sym.returnType
-
-    println(indent + visible + prefix + name + params + ": " + retVal)
-//    for (member <- sym.info.decls)
-//      inspect(member, indent + "  ")
-  }
-
-  def inspectTerm(sym: ru.TermSymbol, indent: String = ""): Unit = {
-    // Determine type
-    var termType = ""
-    sym match {
-      case v if sym.isVal => termType = "val "
-      case v if sym.isVar => termType = "var "
-      case _ => termType = "??? "
-    }
-    // Determine name
-    val name = sym.name.decodedName
-    // Determine type
-    val retVal = sym.typeSignature
-
-    println(indent + termType + name + ": " + retVal)
-  }
-
-  def inspectType(sym: ru.TypeSymbol, indent: String = ""): Unit = {
-    println(indent + sym)
-  }
 }
