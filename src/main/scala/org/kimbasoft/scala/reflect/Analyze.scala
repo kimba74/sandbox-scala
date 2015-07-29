249357ff94c1a81f354526da2315fd471728c807
Steffen Krause <steffen.krause@soabridge.com>
2015-07-28 22:46:56 -0400

    Moved analysis methods to its own object

---
+++ src/main/scala/org/kimbasoft/scala/reflect/Analyze.scala
@@ -0,0 +1,38 @@
+package org.kimbasoft.scala.reflect
+
+import scala.reflect.runtime.{universe => ru}
+
+/**
+ * Missing documentation. 
+ *
+ * @author <a href="steffen.krause@soabridge.com">Steffen Krause</a>
+ * @since 1.0
+ */
+object Analyze {
+
+  def analyze(sym: ru.ClassSymbol): Unit = {
+    println(s"sym                          = $sym")
+    println(s"sym [alternatives]           = ${sym.alternatives}")
+    println(s"sym [annotations]            = ${sym.annotations}")
+    println(s"sym [fullName]               = ${sym.fullName}")
+    println(s"sym [companion]              = ${sym.companion}")
+    println(s"sym [info]                   = ${sym.info}")
+    println(s"    [info - baseClasses]     = ${sym.info.baseClasses}")
+    println(s"    [info - dealias]         = ${sym.info.dealias}")
+    println(s"    [info - decls]           = ${sym.info.decls}")
+    println(s"    [info - erasure]         = ${sym.info.erasure}")
+    println(s"    [info - etaExpand]       = ${sym.info.etaExpand}")
+    println(s"sym [knownDirectSubclasses]  = ${sym.knownDirectSubclasses}")
+    println(s"sym [module]                 = ${sym.module}")
+    println(s"sym [name]                   = ${sym.name}")
+    println(s"sym [overrides]              = ${sym.overrides}")
+    println(s"sym [owner]                  = ${sym.owner}")
+    println(s"sym [pos]                    = ${sym.pos}")
+    println(s"sym [selfType]               = ${sym.selfType}")
+    println(s"sym [thisPrefix]             = ${sym.thisPrefix}")
+    println(s"sym [toType]                 = ${sym.toType}")
+    println(s"sym [toTypeConstructor]      = ${sym.toTypeConstructor}")
+    println(s"sym [typeParams]             = ${sym.typeParams}")
+    println(s"sym [typeSignature]          = ${sym.typeSignature}")
+  }
+}
