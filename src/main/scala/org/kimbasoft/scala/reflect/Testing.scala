70fab84eae34286cbe03e763d4cd46bca407f272
Steffen Krause <slkrause08@gmail.com>
2015-08-09 23:01:04 -0400

    Added Scala approach to checking class implementation

--- src/main/scala/org/kimbasoft/scala/reflect/Testing.scala
+++ src/main/scala/org/kimbasoft/scala/reflect/Testing.scala
@@ -1,5 +1,7 @@
 package org.kimbasoft.scala.reflect
 
+import scala.reflect.runtime.{universe => ru}
+
 /**
  * Missing documentation. 
  *
@@ -8,11 +10,29 @@ package org.kimbasoft.scala.reflect
  */
 object Testing extends App {
 
-  val testClass  = Testing.getClass.getClassLoader.loadClass("org.kimbasoft.scala.reflect.TestClass")
-  val testTrait  = Testing.getClass.getClassLoader.loadClass("org.kimbasoft.scala.reflect.TestTrait")
-  val uTestTrait = Testing.getClass.getClassLoader.loadClass("org.kimbasoft.scala.reflect.UberTestTrait")
+  val strTestClass  = "org.kimbasoft.scala.reflect.TestClass"
+  val strTestTrait  = "org.kimbasoft.scala.reflect.TestTrait"
+  val strUTestTrait = "org.kimbasoft.scala.reflect.UberTestTrait"
 
-  println(s"${testTrait.getName} is assignable from ${testClass.getName} = ${testTrait.isAssignableFrom(testClass)}")
-  println(s"${uTestTrait.getName} is assignable from ${testClass.getName} = ${uTestTrait.isAssignableFrom(testClass)}")
+  val clsTestClass  = Testing.getClass.getClassLoader.loadClass(strTestClass )
+  val clsTestTrait  = Testing.getClass.getClassLoader.loadClass(strTestTrait )
+  val clsUTestTrait = Testing.getClass.getClassLoader.loadClass(strUTestTrait)
 
+  println(s"Java: ${clsTestTrait.getName} is assignable from ${clsTestClass.getName} = ${clsTestTrait.isAssignableFrom(clsTestClass)}")
+  println(s"Java: ${clsUTestTrait.getName} is assignable from ${clsTestClass.getName} = ${clsUTestTrait.isAssignableFrom(clsTestClass)}")
+
+  // Get runtime mirror
+  val rm = ru.runtimeMirror(ru.getClass.getClassLoader)
+
+  val sc = rm.staticClass(strTestClass)
+  val rc = rm.runtimeClass(sc)
+
+  val st = rm.staticClass(strTestTrait)
+  val rt = rm.runtimeClass(st)
+
+  val sut = rm.staticClass(strUTestTrait)
+  val rut = rm.runtimeClass(sut)
+
+  println(s"Scala: ${rt.getName} is assignable from ${rc.getName} = ${rt.isAssignableFrom(rc)}")
+  println(s"Scala: ${rut.getName} is assignable from ${rc.getName} = ${rut.isAssignableFrom(rc)}")
 }
