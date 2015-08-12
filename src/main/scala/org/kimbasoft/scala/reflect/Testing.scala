46370a1af5499416b68c839e26db188bf8db3b70
Steffen Krause <slkrause08@gmail.com>
2015-08-11 21:35:31 -0400

    Added more inline documentation

--- src/main/scala/org/kimbasoft/scala/reflect/Testing.scala
+++ src/main/scala/org/kimbasoft/scala/reflect/Testing.scala
@@ -14,16 +14,18 @@ object Testing extends App {
   val strTestTrait  = "org.kimbasoft.scala.reflect.TestTrait"
   val strUTestTrait = "org.kimbasoft.scala.reflect.UberTestTrait"
 
+  // Java Classes
   val clsTestClass  = Testing.getClass.getClassLoader.loadClass(strTestClass)  // Load Java Class for name
   val clsTestTrait  = Testing.getClass.getClassLoader.loadClass(strTestTrait)  // Load Java Class for name
   val clsUTestTrait = Testing.getClass.getClassLoader.loadClass(strUTestTrait) // Load Java Class for name
 
-  println(s"Java: ${clsTestTrait.getName} is assignable from ${clsTestClass.getName} = ${clsTestTrait.isAssignableFrom(clsTestClass)}")
-  println(s"Java: ${clsUTestTrait.getName} is assignable from ${clsTestClass.getName} = ${clsUTestTrait.isAssignableFrom(clsTestClass)}")
+  println(s"Java : ${clsTestTrait.getName} is assignable from ${clsTestClass.getName} = ${clsTestTrait.isAssignableFrom(clsTestClass)}")
+  println(s"Java : ${clsUTestTrait.getName} is assignable from ${clsTestClass.getName} = ${clsUTestTrait.isAssignableFrom(clsTestClass)}")
 
   // Get Scala RuntimeMirror
   val rm = ru.runtimeMirror(ru.getClass.getClassLoader)
 
+  // Scala ClassSymbol and RuntimeClasses
   val sc = rm.staticClass(strTestClass)   // Load Scala ClassSymbol for name
   val rc = rm.runtimeClass(sc)            // Load corresponding Java class for Scala ClassSymbol
 
