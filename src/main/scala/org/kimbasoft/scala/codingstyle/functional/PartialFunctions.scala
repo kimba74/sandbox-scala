7718d088f3a9f4e03b5b285626ba77d14c6ac02d
Steffen Krause <steffen.krause@soabridge.com>
2015-09-05 23:28:00 -0400

    Corrected minor mistake in string formatting

--- src/main/scala/org/kimbasoft/scala/codingstyle/functional/PartialFunctions.scala
+++ src/main/scala/org/kimbasoft/scala/codingstyle/functional/PartialFunctions.scala
@@ -34,8 +34,8 @@ object PartialFunctions {
     val pf1: PartialFunction[Any,String] = { case i: Int     => s"object $i is an Int" }
     val pf2: PartialFunction[Any,String] = { case d: Double  => s"object $d is a Double" }
     val pf3: PartialFunction[Any,String] = { case b: Boolean => s"object $b is a Boolean" }
-    val pf4: PartialFunction[Any,String] = { case s: String  => s"object \"$s\" is a String" }
-    val pf5: PartialFunction[Any,String] = { case x => s"object \"$x\" of unknown type" }
+    val pf4: PartialFunction[Any,String] = { case s: String  => s"""object "$s" is a String""" }
+    val pf5: PartialFunction[Any,String] = { case x => s"""object "$x" of unknown type""" }
     val pf = pf1 orElse pf2 orElse pf3 orElse pf4 orElse pf5
 
     println(pf("Hello World"))
